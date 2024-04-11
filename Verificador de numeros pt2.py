ae07b69b04c9f1605e5e88a804c018d929d413f5
Raphael Ferraz <102317948+Raphafz@users.noreply.github.com>
2024-04-11 08:22:25 -0300

    Verificador de numeros pt2.py

--- Verificador de numeros pt2.py
+++ Verificador de numeros pt2.py
@@ -5,18 +5,18 @@
 # Matéria: Programação Estruturada
 # Professor: Matheus Almeida
 '''
-10. Um programa que verifique se um numero é Positivo e par, positivo e múltiplo
+10. Um programa que verifique se um número é Positivo e par, positivo e múltiplo
     de três, negativo e ímpar, Zero ou se não é nenhum desses.
 '''
 
 
 print('=============================================')
-print('''Bem vindo ao verificador de numeros!\n
+print('''Bem vindo ao verificador de números!\n
  Vamos verificar se o número é:
     -Positivo e par
     -Positivo e múltiplo de 3
     -Negativo e ímpar
-    -Se o número é Zero      
+    -Se o número é zero      
 ''')
 print('=============================================')
 numero = float(input('Digite o número que será verificado: '))
