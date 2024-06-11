710b3880c73fa049b0c210532261d7d696bbbcd4
Raphael Ferraz <102317948+Raphafz@users.noreply.github.com>
2024-06-11 11:14:11 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,12 @@
+alunos = []
+
+for nomes in range(1,20):
+    nomes = input('Escreva os nomes dos alunos: ')
+    if nomes == 'fim': 
+        print(alunos)
+        
+    alunos.append(nomes)
+    
+        
+       
+
